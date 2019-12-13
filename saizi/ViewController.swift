51d158c8ca95672f56f7d49e91f3794fe79e8fff
20181105879 <2605753145@qq.com>
2019-12-13 09:47:23 +0800

    Initial Commit

---
+++ saizi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  saizi
+//
+//  Created by s20181105879 on 2019/12/13.
+//  Copyright © 2019 s20181105879. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
